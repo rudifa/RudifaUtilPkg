f411a447072c3e57f7e64744f9a881559bd71ec0
Rudi Farkas <rudi.farkas@gmail.com>
2020-09-23 20:08:38 +0200

    CodableExt: add encode/decode with String; expand tests

--- Sources/RudifaUtilPkg/CodableExt.swift
+++ Sources/RudifaUtilPkg/CodableExt.swift
@@ -1,5 +1,5 @@
 //
-//  CodableExt.swift v.0.2.0
+//  CodableExt.swift v.0.2.1
 //  RudifaUtilPkg
 //
 //  Created by Rudolf Farkas on 23.06.19.
@@ -8,49 +8,108 @@
 
 import Foundation
 
-/**
- Extensions originated in https://gist.github.com/StanislavK/e763cdc9fbe92f62f3c9dbd648e7e7ad
+// Extensions inspired by https://gist.github.com/StanislavK/e763cdc9fbe92f62f3c9dbd648e7e7ad
 
+/**
  Usage examples
 
-     struct Language: Codable {
-         var name: String
-         var version: String
-     }
+    struct Language: Codable {
+        var name: String
+        var version: String
+    }
 
-     // create an instance
-     let language = Language(name: "Swift", version: "4")
+    // create an instance
+    let language = Language(name: "Swift", version: "4")
 
-     // encode
-     if let data = try? language.encode() {
+    // encode to Data?
+    if let data: Data = language.encode() {
          // use data here
-     }
 
-     // decode
-     if let lang = try? Language.decode(from: data!) {
-         // use lang here
-     }
+        // decode from Data
+        if let lang = Language.decode(from: data) {
+            // use lang here
+        } else {
+            // handle decode error
+        }
+    } else {
+        // handle encode error
+    }
+
+    // encode to String?
+    if let string: String = language.encode() {
+        // use string here
+
+        // decode from String
+        if let lang = Language.decode(from: string) {
+            // use lang here
+        } else {
+            // handle decode error
+        }
+    } else {
+        // handle encode error
+    }
  */
 extension Encodable {
-    /// Encodes self into Data
-    ///
+    /// Encode self into Data
+    /// THROWING VERSION REMOVED
     /// - Parameter encoder: defaults to JSONEncoder
     /// - Returns: encoded Data
     /// - Throws: on error
-    public func encode(_ encoder: JSONEncoder = JSONEncoder()) throws -> Data {
-        return try encoder.encode(self)
+    //    public func encode(_ encoder: JSONEncoder = JSONEncoder()) throws -> Data {
+    //        return try encoder.encode(self)
+    //    }
+
+    /// Encode self into Data?
+    /// - Parameter encoder: defaults to JSONEncoder
+    /// - Returns: Data?
+    public func encode(_ encoder: JSONEncoder = JSONEncoder()) -> Data? {
+        return try? encoder.encode(self)
+    }
+
+    /// Encode self into String?
+    /// - Parameter encoder: defaults to JSONEncoder
+    /// - Returns: String?
+    public func encode(_ encoder: JSONEncoder = JSONEncoder()) -> String? {
+        if let data: Data = encode(encoder) {
+            return String(data: data, encoding: .utf8)
+        }
+        return nil
     }
 }
 
+/**
+ Usage examples: see `extension Encodable`
+ */
 extension Decodable {
-    /// Decodes Data into Self
-    ///
+    /// Decode Data into Self
+    /// THROWING VERSION REMOVED
     /// - Parameters:
     ///   - decoder: defaults to JSONDecoder
     ///   - data: previously encoded Data
     /// - Returns: a Self value
     /// - Throws: on error
-    public static func decode(with decoder: JSONDecoder = JSONDecoder(), from data: Data) throws -> Self {
-        return try decoder.decode(Self.self, from: data)
+    //    public static func decode(with decoder: JSONDecoder = JSONDecoder(), from data: Data) throws -> Self {
+    //        return try decoder.decode(Self.self, from: data)
+    //    }
+
+    /// Decode Data into Self?
+    /// - Parameters:
+    ///   - decoder: defaults to JSONDecoder
+    ///   - data: previously encoded Data
+    /// - Returns: Self?
+    public static func decode(with decoder: JSONDecoder = JSONDecoder(), from data: Data) -> Self? {
+        return try? decoder.decode(Self.self, from: data)
+    }
+
+    /// Decode String into Self?
+    /// - Parameters:
+    ///   - decoder: defaults to JSONDecoder
+    ///   - string: previously encoded String
+    /// - Returns: Self?
+    public static func decode(with decoder: JSONDecoder = JSONDecoder(), from string: String) -> Self? {
+        if let data = string.data(using: .utf8) {
+            return try? decoder.decode(Self.self, from: data)
+        }
+        return nil
     }
 }
