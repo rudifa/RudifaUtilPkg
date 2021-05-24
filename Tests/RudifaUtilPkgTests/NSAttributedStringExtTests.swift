dca92c173e55f70dc32e0e6e8831605744adc829
Rudi Farkas <rudi.farkas@gmail.com>
2021-05-24 14:55:27 +0200

    NSAttributedStringExtTests: clean up

--- Tests/RudifaUtilPkgTests/NSAttributedStringExtTests.swift
+++ Tests/RudifaUtilPkgTests/NSAttributedStringExtTests.swift
@@ -17,13 +17,15 @@ class NSAttributedStringExtTests: XCTestCase {
         let price = "24.99"
         let currency = "€ / day"
         do {
-            let pcAttributedString: NSAttributedString = NSAttributedString(stringsWithStyle: [(price, .title3), (currency, .footnote)], separator: " ")
+            let pcAttributedString = NSAttributedString(stringsWithStyle: [(price, .title3),
+                                                                           (currency, .footnote)], separator: " ")
             XCTAssertEqual(pcAttributedString.string, "24.99 € / day")
             XCTAssertEqual(pcAttributedString.length, 13)
             XCTAssertEqual(pcAttributedString.size(), CGSize(width: 95.9931640625, height: 23.8671875))
         }
         do {
-            let pcAttributedString: NSAttributedString = NSAttributedString(stringsWithStyle: [(price, .title1), (currency, .title3)], separator: " ")
+            let pcAttributedString = NSAttributedString(stringsWithStyle: [(price, .title1),
+                                                                           (currency, .title3)], separator: " ")
             XCTAssertEqual(pcAttributedString.string, "24.99 € / day")
             XCTAssertEqual(pcAttributedString.length, 13)
             XCTAssertEqual(pcAttributedString.size(), CGSize(width: 137.123046875, height: 33.4140625))
